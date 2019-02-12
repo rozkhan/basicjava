4e2ef53e0ac61a953f359684b9f9cca096920f1e
firoz aalam <firozaalam99@gmail.com>
2019-02-12 20:34:46 +0530

    test
    basic java

---
+++ TestJava/src/java/com/test/CompareTo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+/**
+ *
+ * @author Firoz
+ */
+public class CompareTo {
+    public static void main(String[] args) {
+        
+        String txt1="firoz";
+        String txt2="firoz";
+        System.out.println("Result :  "+txt1.compareTo(txt2));
+    }
+    
+}
---
+++ TestJava/src/java/com/test/T.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+/**
+ *
+ * @author Firoz
+ */
+
+public class T {
+
+	public static void main(String[] args) {
+		String x = "abc";
+		String y = "abc";
+		x=x.concat(y);
+		System.out.print(x);
+	}
+}
+
